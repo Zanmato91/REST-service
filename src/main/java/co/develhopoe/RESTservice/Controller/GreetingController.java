981c17514b8020c300fe16b6f8f2e604df01ab99
Alessandro Marinelli <a.marinelli1991@gmail.com>
2022-05-23 14:41:04 +0200

    Initial commit

---
+++ src/main/java/co/develhopoe/RESTservice/Controller/GreetingController.java
@@ -0,0 +1,18 @@
+package co.develhopoe.RESTservice.Controller;
+import java.util.concurrent.atomic.AtomicLong;
+import co.develhopoe.RESTservice.Greetings;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greetings greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greetings(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/test/java/co/develhopoe/RESTservice/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhopoe.RESTservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
